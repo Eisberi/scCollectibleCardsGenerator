767c343165741017cf303cb752530a5183a1e53e
Eisberi <maximilianschardt2048@gmail.com>
2026-05-25 18:54:58 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import io
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import requests
 
 MC_SKIN_RENDER_URL = "https://nmsr.nickac.dev/fullbody/"
@@ -10,7 +10,44 @@ def scale_image(image_to_rescale: Image, width: int) -> Image:
 
     return image_to_rescale.resize(new_size, Image.Resampling.NEAREST)
 
-# ziel ist passende width -> factor der verkleinerung auf height anwenden
+def create_hand_card(username_input: str, description: str = "", player_image_width: int = 32):
+    # Setup
+    hand_card = Image.open("template_hand.png")
+    font = ImageFont.truetype("minecraftFont.ttf", 12)
+
+    # Retrieving and scaling the player image
+    player_image = None
+    player_image_response = requests.get(MC_SKIN_RENDER_URL + username_input)
+
+    if player_image_response.status_code == 200:
+        player_image = Image.open(io.BytesIO(player_image_response.content))
+    else:
+        player_image = Image.new("RGBA", (64, 80))
+
+    player_image = scale_image(player_image, player_image_width).convert("RGBA")
+
+    # Writing the description
+    description_image = Image.new("RGBA", (72, 60))
+
+    temp_draw = ImageDraw.Draw(description_image)
+    temp_draw.text((0, 0), description, font=font)
+
+    # Writing the username
+    username_image = Image.new("RGBA", (88, 12))
+
+    temp_draw = ImageDraw.Draw(username_image)
+    temp_draw.text((0, 0), username_input, font=font)
+
+    # Assembling the image
+    player_image_x = int(96 + (64 - player_image.size[0]) / 2)
+    player_image_y = int(56 + (80 - player_image.size[1]) / 2)
+    hand_card.paste(player_image, (player_image_x, player_image_y), player_image)
+
+    hand_card.paste(description_image, (92, 140), description_image)
+    hand_card.paste(username_image, (84, 36-1), username_image)
+
+    # Displaying a preview
+    st.image(hand_card)
 
 if __name__ == "__main__":
     title = st.title("Collectible Card Generator")
@@ -18,14 +55,8 @@ if __name__ == "__main__":
     username_input = st.text_input("Username")
     description_input = st.text_area("Description")
 
-    image_scale = st.number_input("Image scale", min_value=8, max_value=256)
+    image_scale = st.number_input("Player's image scale", min_value=8, max_value=256)
 
     image_response = requests.get(MC_SKIN_RENDER_URL + username_input)
 
-    image = None
-    if image_response.status_code == 200:
-        image = Image.open(io.BytesIO(image_response.content))
-        image = scale_image(image, image_scale)
-    preview_image = scale_image(image, 256)
-    st.image(image)
-    st.image(preview_image)
+    create_hand_card(username_input, description=description_input, player_image_width=image_scale)
