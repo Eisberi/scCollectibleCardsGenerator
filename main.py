78e1888e928e7d3fc65f2b79aef48c0830563c1e
Eisberi <maximilianschardt2048@gmail.com>
2026-05-26 16:08:32 +0200

    Added season rings

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import streamlit as st
 from PIL import Image, ImageFont, ImageDraw
 import requests
 
-MC_SKIN_RENDER_URL = "https://nmsr.nickac.dev/fullbody/"
+MC_SKIN_RENDER_URL = "https://nmsr.nickac.dev/"
+
+SEASON_SELECTION_MAP = {
+    "Season 1": "s1",
+    "Season 2": "s2",
+    "Season 3": "s3",
+}
+
+ALL_SEASONS = ["s1", "s2", "s3"]
 
 def scale_image(image_to_rescale: Image, width: int) -> Image:
     new_size = (width, int(image_to_rescale.size[1] * (width / image_to_rescale.size[0])))
@@ -14,7 +22,7 @@ def scale_image(image_to_rescale: Image, width: int) -> Image:
 
 @st.cache_data
 def load_skin(username: str) -> Image:
-    player_image_response = requests.get(MC_SKIN_RENDER_URL + username_input)
+    player_image_response = requests.get(MC_SKIN_RENDER_URL + "fullbody/" + username_input)
 
     if player_image_response.status_code == 200:
         player_image = Image.open(io.BytesIO(player_image_response.content))
@@ -24,8 +32,19 @@ def load_skin(username: str) -> Image:
     return player_image
 
 @st.cache_data
+def load_skin_head_texture(username: str) -> Image:
+    player_image_response = requests.get(MC_SKIN_RENDER_URL + "face/" + username_input)
+
+    if player_image_response.status_code == 200:
+        player_image = Image.open(io.BytesIO(player_image_response.content))
+    else:
+        player_image = Image.new("RGBA", (64, 64))
+
+    return player_image
+
+@st.cache_data
 def load_description(description: str) -> Image:
-    font = ImageFont.truetype("minecraftFont.ttf", 16)
+    font = ImageFont.truetype("sprites/minecraftFont.ttf", 16)
     description_image = Image.new("RGBA", (144, 120))
 
     temp_draw = ImageDraw.Draw(description_image)
@@ -35,7 +54,7 @@ def load_description(description: str) -> Image:
 
 @st.cache_data
 def load_username(username: str) -> Image:
-    titleFont = ImageFont.truetype("minecraftFont.ttf", 32)
+    titleFont = ImageFont.truetype("sprites/minecraftFont.ttf", 32)
     username_image = Image.new("RGBA", (176, 24))
 
     temp_draw = ImageDraw.Draw(username_image)
@@ -43,14 +62,34 @@ def load_username(username: str) -> Image:
 
     return username_image
 
-@st.cache_data
-def create_hand_card(username_input: str, description: str = "", player_image_width: int = 32):
+def load_seasons(seasons: list[str]) -> Image:
+    st.write("hello")
+    seasons_image = Image.new("RGBA", (512, 512))
+    for season in ALL_SEASONS:
+        st.write("hello world", season)
+        if seasons.__contains__(season):
+            temp_image = Image.open("sprites/scSeasonRings/" + season + "_filled.png")
+            seasons_image.paste(temp_image, (0, 0), temp_image)
+            st.write(season + "writtenBecauseCool")
+        else:
+            temp_image = Image.open("sprites/scSeasonRings/" + season + "_empty.png")
+            seasons_image.paste(temp_image, (0, 0), temp_image)
+            st.write(season + "written")
+
+    return seasons_image
+
+def create_hand_card(username_input: str, seasons_input: list[str], description: str = "", player_image_width: int = 32):
     # Setup
-    hand_card = Image.open("template_hand.png")
+    hand_card = Image.open("sprites/template_hand.png")
 
     # Retrieving the components
     player_image = load_skin(username_input)
     player_image = scale_image(player_image, player_image_width).convert("RGBA")
+
+    player_face_image = load_skin_head_texture(username_input)
+    player_face_image = scale_image(player_face_image, 24).convert("RGBA")
+
+    seasons_image = load_seasons(seasons_input)
 
     description_image = load_description(description)
     username_image = load_username(username_input)
@@ -59,6 +98,9 @@ def create_hand_card(username_input: str, description: str = "", player_image_wi
     player_image_x = int(192 + (128 - player_image.size[0]) / 2)
     player_image_y = int(112 + (160 - player_image.size[1]) / 2)
     hand_card.paste(player_image, (player_image_x, player_image_y), player_image)
+    hand_card.paste(player_face_image, (324, 388), player_face_image)
+
+    hand_card.paste(seasons_image, (0, 0), seasons_image)
 
     hand_card.paste(description_image, (184, 280), description_image)
     hand_card.paste(username_image, (168, 72-2), username_image)
@@ -72,8 +114,12 @@ if __name__ == "__main__":
     username_input = st.text_input("Username")
     description_input = st.text_area("Description")
 
+    seasons_input = st.segmented_control("Seasons", SEASON_SELECTION_MAP, selection_mode="multi")
+
+    st.write(seasons_input)
+
     image_scale = st.number_input("Player's image scale", min_value=8, max_value=256, value=80)
 
     image_response = requests.get(MC_SKIN_RENDER_URL + username_input)
 
-    create_hand_card(username_input, description=description_input, player_image_width=image_scale)
+    create_hand_card(username_input, seasons_input=seasons_input, description=description_input, player_image_width=image_scale)
